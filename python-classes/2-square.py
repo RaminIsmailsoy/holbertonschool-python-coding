0eb84d3f998ca90e6c83ce381f50e8f691b594c0
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-23 09:20:41 +0000

    all

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,10 +4,15 @@
 
 class Square:
     """A class representing Square"""
+
     def __init__(self, size=0):
         """Instantiation with a given size"""
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size * self.__size
