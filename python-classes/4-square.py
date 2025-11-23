67fb0c287b42568bc2892458bcffa35e34d99d1c
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-23 09:23:03 +0000

    all

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -4,28 +4,24 @@
 
 class Square:
     """A class representing Square"""
+
     def __init__(self, size=0):
         """Instantiation with a given size"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size  # use setter for validation
 
     @property
     def size(self):
-        """Getter for the size of square"""
+        """Getter for the size of the square"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Setter for the size of square"""
+        """Setter for the size of the square"""
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def area(self):
-        """Return of the area of square"""
-        return self.__size**2
+        """Return the area of the
