c547481969a0ab3b14e9deebd29acc24b54f746f
RaminIsmayilov <ramin.ismayilov@gmail.com>
2025-11-23 09:49:00 +0000

    all

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""Square module.
-
-This module contains a class that defines a square.
-"""
+"""Square module"""
 
 
 class Square:
-    """Defines a square."""
+    """Defines a square"""
 
     def __init__(self, size):
         """Initialize the square with a size."""
